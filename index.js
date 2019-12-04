4cf02a5e23502a15c24ada1fef76314ca09af502
nadia2612 <YOUR@EMAIL.com>
2019-12-04 17:08:40 +0100

    Data

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 // const db = require('./db')
 // const team=require("./team/model")
-const teamRouter=require("./team/router")
-
 const app = express()
+const teamRouter=require("./team/router")
+const bodyParser=require("body-parser")
+const jsonParser = bodyParser.json()
+
+app.use(jsonParser)
+
+
 const port = process.env.PORT || 4003
 app.use(teamRouter)
 
--- team/router.js
+++ team/router.js
@@ -1,20 +1,25 @@
-const {Router}=require("express")
-const Team=require("./model")
+const { Router } = require("express");
+const Team = require("./model");
 
-const router = new Router()
-router.get('/team', (req, res,next) => {
-Team.findAll()
-  .then((teams)=>{
-    console.log("found teams", teams);
-    
-    res.send(teams)}
-    )
-.catch(err => {
-  console.log("got here");
-  
-  next()}
-  )
+const router = new Router();
+router.get("/team", (req, res, next) => {
+  Team.findAll()
+    .then(teams => {
+      res.send(teams);
+    })
+    .catch(err => {
+      console.log("got here");
 
-}
-)
-module.exports = router
+      next(err);
+    });
+});
+router.post("/team", (req, res, next) =>
+  Team.create(req.body)
+    .then(result => {
+      res.json(result);
+    })
+    .catch(err => {
+      next(err);
+    })
+);
+module.exports = router;
